777a405504dff5eba3f635adafff3ba4ea3f9df5
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-10-01 22:35:02 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine();
+            int n = input.Length;
+            string result = "";
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (input[i] == ':')
+                {
+                    result += '*';
+                }
+                else
+                {
+                    result += input[i];
+                }
+            }
+            for (int i = n / 2; i < n; i++)
+            {
+                if (input[i] == '!')
+                {
+                    result += '.';
+                }
+                else
+                {
+                    result += input[i];
+                }
+            }
+
+            Console.WriteLine("Результат: " + result);
+        }
+    }
+}
